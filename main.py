586dc2b89ba2a446e38b37a6c03b88ae611d0582
KairzhanovMurat <murat99010@gmail.com>
2023-08-07 17:20:45 +0600

    updates

--- main.py
+++ main.py
@@ -1,14 +1,60 @@
-from fastapi import FastAPI, File, UploadFile
-import pandas as pd
+import uvicorn
+from fastapi import FastAPI, File, UploadFile, Request
+from fastapi.templating import Jinja2Templates
+import os
+import services
+from fastapi.responses import HTMLResponse
 
-app = FastAPI(docs_url='/')
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+static_path = os.path.join(os.path.dirname(__file__), 'static')
 
 
-@app.post('/BOM')
-async def items_count(BOM: UploadFile = File(...)):
-    file = await BOM.read()
-    items = pd.read_excel(file)
-    end = len(items) - 2
-    articuls = items.iloc[9:end, 3]
-    quantities = items.iloc[9:end, 14]
-    return dict(zip(articuls, quantities))
+@app.get('/remains')
+def remains():
+    return services.get_remains()
+
+
+@app.post('/calculate_routers_total')
+async def calc(bom: UploadFile = File(...)):
+    items = await services.parse_excel(bom)
+    routers = services.calculate_routers_total(items)
+    return {'num routers': routers}
+
+
+@app.post('/calculate_max_routers')
+async def calc_res(bom: UploadFile = File(...)):
+    items = await services.parse_excel(bom)
+    return services.calculate_num_routers_for_articul(items)
+
+
+@app.post('/calculate_single_router')
+async def calc_res(bom: UploadFile = File(...)):
+    items = await services.parse_excel(bom)
+    return services.calculate_articuls_for_one(items)
+
+
+@app.post('/parse_data_1c')
+async def parse(data: UploadFile = File(...)):
+    return await services.fetch_data_from_1c(data)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/show_tables", response_class=HTMLResponse)
+async def show_tables(request: Request, bom: UploadFile = File(...)):
+    items = await services.parse_excel(bom)
+    routers_total = services.calculate_articuls_for_one(items)
+    single_router = services.calculate_num_routers_for_articul(items)
+
+    return templates.TemplateResponse(
+        "tables.html",
+        {"request": request, "routers_total": routers_total, "single_router": single_router},
+    )
+
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host="0.0.0.0", port=8000, reload=True)
--- services.py
+++ services.py
@@ -1,24 +1,96 @@
+import io
+from itertools import groupby
+
+import pandas as pd
 import requests
 from fastapi import UploadFile, File
-import pandas as pd
 
 
-async def items_count(BOM: UploadFile = File(...)):
-    file = await BOM.read()
-    items = pd.read_excel(file)
+async def parse_excel(bom: UploadFile = File(...)) -> dict:
+    content = await bom.read()
+    with pd.ExcelFile(io.BytesIO(content)) as xls:
+        sheet_name = xls.sheet_names[0]
+        items = pd.read_excel(xls, sheet_name)
     end = len(items) - 2
-    names = items.iloc[9:end, 4]
+    articuls = items.iloc[9:end, 3]
     quantities = items.iloc[9:end, 14]
-    return dict(zip(names, quantities))
+    return dict(zip(articuls, quantities))
 
 
-async def fetch_data(*args, **kwargs):
-    ...
+def get_remains() -> dict:
+    # from collections import defaultdict
+    remains = requests.get('http://10.10.2.180:5001/ostatki.json').json()
+    dict_list = [{'articul': d['artukul'], 'quantity': int(d['quantity'])} for d in remains]
+    # res_dict = defaultdict(int)
+    # for dictionary in dict_list:
+    #     articul = dictionary['articul']
+    #     quantity = dictionary['quantity']
+    #     res_dict[articul] += quantity
+    # return dict(res_dict)
+    dict_list.sort(key=lambda x: x['articul'])
+    grouped_data = groupby(dict_list, key=lambda x: x['articul'])
+    result_dict = {key: max(group, key=lambda x: x['quantity'])['quantity'] for key, group in grouped_data}
+    return result_dict
+
+
+def calculate_articuls_for_one(bom_items: dict) -> dict:
+    remains = get_remains()
+    for articul in bom_items:
+        if remains.get(articul) is not None:
+            difference = remains[articul] - bom_items[articul]
+            if difference < 0:
+                bom_items[articul] = f'не хватает, нужно дозаказать {abs(difference)} шт.'
+            else:
+                bom_items[articul] = f' осталось {difference}, с учетом вычета'
+        else:
+            bom_items[articul] = f'отсутствует на складе, нужно заказать {bom_items[articul]} шт'
+    return bom_items
+
+
+def calculate_routers_total(bom_items: dict) -> int:
+    remains = get_remains()
+    routers_available = []
+    for articul in bom_items:
+        if remains.get(articul) is not None:
+            routers_max = remains[articul] // bom_items[articul]
+        else:
+            return 0
+        routers_available.append(routers_max)
+    return min(routers_available)
+
+
+def calculate_num_routers_for_articul(items: dict) -> dict:
+    remains = get_remains()
+    for articul in items:
+        if remains.get(articul) is not None:
+            routers_num = remains[articul] // items[articul]
+            if routers_num >= 1:
+                remainder = remains[articul] % items[articul]
+                items[articul] = f'хватит на {routers_num} роутеров, остаток: {remainder} шт.'
+            else:
+                items[articul] = f'хватит на {routers_num} роутеров.'
+        else:
+            needed_amount = items[articul]
+            items[articul] = f'отсутствует, нужно заказать {needed_amount} шт.'
+    return items
+
+
+async def fetch_data_from_1c(data: UploadFile = File(...)) -> dict:
+    content = await data.read()
+    with pd.ExcelFile(io.BytesIO(content)) as xls:
+        sheet_name = xls.sheet_names[0]
+        items = pd.read_excel(xls, sheet_name)
+    end = len(items) - 1
+    articuls = items.iloc[15:end, 2]
+    quantities = items.iloc[15:end, 8]
+    payload = dict()
+    for a, q in zip(articuls, quantities):
+        if payload.get(a) is None:
+            payload[a] = q
+        else:
+            payload[a] += q
+    return payload
 
 
 def generate_doc(*args, **kwargs):
     ...
-
-
-def calculate():
-    pass
